104965a7488ca24b3992315d123e162053eb4308
glassjoseph <glassjoseph@gmail.com>
2017-05-17 11:13:31 -0600

    extract station object creation into model
    - need to change create_stations to be find_or_create_by

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,14 @@
 class Station < ActiveRecord::Base
 
 
+  def self.create_stations(response)
+    response[:fuel_stations].each do |station|
+      Station.create(name: station[:station_name],
+                   address: "#{station[:street_address]}, #{station[:city]}, #{station[:state]}, #{station[:zip]}",
+                   fuel_types: station[:fuel_type_code],
+                   distance: station[:distance],
+                   access_days_time: station[:access_days_time])
+    end
+  end
+
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -4,16 +4,6 @@ class NrelService
     response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&fuel_type=ELEC,LPG&api_key=#{ENV['nrel_api_key']}")
 
     response = JSON.parse(response.body, symbolize_names: true)
-    response[:fuel_stations].each do |station|
-      Station.create(name: station[:station_name],
-                     address: "#{station[:street_address]}, #{station[:city]}, #{station[:state]}, #{station[:zip]}",
-                     fuel_types: station[:fuel_type_code],
-                     distance: station[:distance],
-                     access_days_time: station[:access_days_time])
-     end
+    Station.create_stations(response)
   end
-
-
-
-
 end
