4d0a7b3015b0862cb7b7a0d38929b46940401a1e
glassjoseph <glassjoseph@gmail.com>
2017-05-17 10:51:52 -0600

    clean spec test
    * still need to extract Station object creation from NrelService
    * need to create station model test

--- spec/feature/user_can_see_closest_stations_spec.rb
+++ spec/feature/user_can_see_closest_stations_spec.rb
@@ -3,16 +3,11 @@ require 'rails_helper'
 RSpec.feature "User can see closest stations" do
   it "user can search for closest electric and propane stations" do
     visit '/'
-    # And I fill in the search form with 80203
-    # And I click "Locate"
-    # Then I should be on page "/search" with parameters visible in the url
+
     fill_in "q", with: "80203"
     click_on "Locate"
-# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC,LPG&api_key=ENV["nrel_api_key"]
-
     expect(current_path).to eq('/search')
 
-    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
     expect(page).to_not have_content("Fuel Types: BD")
     expect(page).to_not have_content("Fuel Types: CNG")
     expect(page).to_not have_content("Fuel Types: HY")
@@ -23,6 +18,5 @@ RSpec.feature "User can see closest stations" do
     expect(page).to have_content("Fuel Types: ELEC")
     expect(page).to have_content("Distance: 0.31422")
     expect(page).to have_content("Access Times: 24 hours daily")
-
   end
 end
