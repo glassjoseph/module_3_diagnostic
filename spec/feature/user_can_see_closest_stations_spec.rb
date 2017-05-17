d2ece2d8095342c00a206fe454d4688f2aa7f835
glassjoseph <glassjoseph@gmail.com>
2017-05-17 09:56:12 -0600

    test for station data

--- spec/feature/user_can_see_closest_stations_spec.rb
+++ spec/feature/user_can_see_closest_stations_spec.rb
@@ -1,17 +1,15 @@
-# As a user
-# When I visit "/"
-# And I fill in the search form with 80203
-# And I click "Locate"
-# Then I should be on page "/search" with parameters visible in the url
-# Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-# And the stations should be limited to Electric and Propane   ELEC, LPG (Propane)
-# And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
 
 require 'rails_helper'
 
+# As a user
+# When I visit "/"
 RSpec.feature "User can see closest stations" do
   it "user can search for closest stations" do
     visit '/'
+    # And I fill in the search form with 80203
+    # And I click "Locate"
+    # Then I should be on page "/search" with parameters visible in the url
+    fill_in "q", with: "80203"
     click_on "Locate"
 
     #
@@ -19,10 +17,24 @@ RSpec.feature "User can see closest stations" do
     # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC,LPG&api_key=ENV["nrel_api_key"]
 
     # GET /api/alt-fuel-stations/v1/nearest.format?parameters
-    # GET /api/alt-fuel-stations/v1/nearest.json?    &api_key=YOUR_KEY_HERE'
-    # expect(current_path).to eq('/search')
-    # expect(current_path).to have_content('/search')
 
+    expect(current_path).to eq('/search')
+
+
+
+
+    # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
+    # And the stations should be limited to Electric and Propane   ELEC, LPG (Propane)
+    expect(page).to_not have_content("Fuel Types: BD")
+    expect(page).to_not have_content("Fuel Types: CNG")
+    expect(page).to_not have_content("Fuel Types: HY")
+    expect(page).to_not have_content("Fuel Types: LNG")
+    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+    expect(page).to have_content("Name: UDR")
+    expect(page).to have_content("Address: 800 Acoma St, Denver, CO, 80204")
+    expect(page).to have_content("Fuel Types: UDR")
+    expect(page).to have_content("Distance: 0.31422")
+    expect(page).to have_content("Access Times: UDR")
 
   end
 end
