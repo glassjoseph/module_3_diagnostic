fca96d1d48f831f1bffac7cd390b7df16813e258
glassjoseph <glassjoseph@gmail.com>
2017-05-17 10:05:22 -0600

    add station model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170517160357) do
+
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "fuel_types"
+    t.float    "distance"
+    t.string   "access_days_time"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
