adc86f71732960714fcc09fa384ccb888d4afa5c
glassjoseph <glassjoseph@gmail.com>
2017-05-17 10:47:19 -0600

    add index view for stations

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -4,13 +4,13 @@ class NrelService
     response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&fuel_type=ELEC,LPG&api_key=#{ENV['nrel_api_key']}")
 
     response = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
     response[:fuel_stations].each do |station|
       Station.create(name: station[:station_name],
                      address: "#{station[:street_address]}, #{station[:city]}, #{station[:state]}, #{station[:zip]}",
                      fuel_types: station[:fuel_type_code],
                      distance: station[:distance],
                      access_days_time: station[:access_days_time])
+     end
   end
 
 
--- spec/feature/user_can_see_closest_stations_spec.rb
+++ spec/feature/user_can_see_closest_stations_spec.rb
@@ -1,40 +1,28 @@
-
 require 'rails_helper'
 
-# As a user
-# When I visit "/"
 RSpec.feature "User can see closest stations" do
-  it "user can search for closest stations" do
+  it "user can search for closest electric and propane stations" do
     visit '/'
     # And I fill in the search form with 80203
     # And I click "Locate"
     # Then I should be on page "/search" with parameters visible in the url
     fill_in "q", with: "80203"
     click_on "Locate"
-
-    #
-
-    # https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC,LPG&api_key=ENV["nrel_api_key"]
-
-    # GET /api/alt-fuel-stations/v1/nearest.format?parameters
+# https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=80203&fuel_type=ELEC,LPG&api_key=ENV["nrel_api_key"]
 
     expect(current_path).to eq('/search')
 
-
-
-
     # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-    # And the stations should be limited to Electric and Propane   ELEC, LPG (Propane)
     expect(page).to_not have_content("Fuel Types: BD")
     expect(page).to_not have_content("Fuel Types: CNG")
     expect(page).to_not have_content("Fuel Types: HY")
     expect(page).to_not have_content("Fuel Types: LNG")
-    # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
+
     expect(page).to have_content("Name: UDR")
     expect(page).to have_content("Address: 800 Acoma St, Denver, CO, 80204")
-    expect(page).to have_content("Fuel Types: UDR")
+    expect(page).to have_content("Fuel Types: ELEC")
     expect(page).to have_content("Distance: 0.31422")
-    expect(page).to have_content("Access Times: UDR")
+    expect(page).to have_content("Access Times: 24 hours daily")
 
   end
 end
